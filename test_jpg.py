8f4706c20eeedae623afcb43815373865a674e62
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-02-03 17:41:27 +0300

    test_convert__jpg updated -APIDON-45

--- test_jpg.py
+++ test_jpg.py
@@ -6,8 +6,8 @@ from apidon_bridge.convert_jpg import app
 client = TestClient(app)
 
 def test_convert_jpg():
-    with open('image/aa.jpg','rb') as img_file:
-        response= client.post("/convert_jpg/", files = {"file":img_file})
+    with open('image/aa.jpg','rb') as img_fil:
+        response= client.post("/convert_jpg/", files = {"file":img_fil})
 
         assert response.status_code == 200
         assert "application/json" in response.headers['content-type']
