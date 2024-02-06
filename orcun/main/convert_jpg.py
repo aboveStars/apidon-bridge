3fb434dfa284bc17369a65bca641d5f3aa291814
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-02-06 12:00:13 +0300

    orcun improved -APIDON-45

--- orcun/main/convert_jpg.py
+++ orcun/main/convert_jpg.py
@@ -1,27 +1,41 @@
-from fastapi import FastAPI , HTTPException, File, UploadFile
+from fastapi import FastAPI , HTTPException, File, UploadFile, Form ,Response
 from fastapi.responses import JSONResponse
 import os
-import io
+import io 
 from PIL import Image
-
+import requests
+from io import BytesIO
+import time
 app = FastAPI()
 
-DIRECTORY= "/Users/mustafaorcunucgun/Documents/apidon/apidon_bridge/converted_images"
-os.makedirs(DIRECTORY, exist_ok=True)
 
-@app.post("/convert_jpg/")
-async def convert_jpg(file: UploadFile = File(...)):
+os.makedirs("orcun/ConvertedImages", exist_ok=True)
+
+
+
+
+@app.post("/classify/")
+async def classify(image_url: str = Form(...)):
+    response = requests.get(image_url)
+
+    if response.status_code != 200:
+        
+        raise HTTPException(status_code=422 , detail="Image could not be accessed")
+    
+    file = Image.open(BytesIO(response.content))   
     try:
-        content = await file.read()
-        image = Image.open(io.BytesIO(content))
-
-        new_image_path = os.path.join(DIRECTORY , f"{file.filename.rsplit('.',1)[0]}.jpg")
-        image.convert('RGB').save(new_image_path , 'JPEG')
-
+        timestamp = int(time.time()) # for different file names
+        new_jpg=file.convert('RGB') 
+        new_path=f"orcun/convertedImages/{timestamp}.jpg"
+        new_jpg.save(new_path,'JPEG')
+        
+       
+        
         return JSONResponse(content = ["cat","tiger","giraffe","puma","lion"])
     
     except Exception as e:
         raise HTTPException(status_code=500 , detail=str(e))
+    
     
 
 @app.get("/")
